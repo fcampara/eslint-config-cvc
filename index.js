6854699cf67de653e28af89b1770d0cebd6c3abb
Felipecs <fcamparasilva@gmail.com>
2021-12-01 13:05:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const base = require('./lib/base.js');
+const next = require('./lib/next.js');
+const react = require('./lib/react.js');
+
+module.exports = {
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['simple-import-sort', 'import'],
+  rules: {
+    ...base,
+    ...next,
+    ...react,
+  },
+  settings: {
+    react: {
+      // Tells eslint-plugin-react to automatically detect the version of React to use
+      version: 'detect',
+    },
+  },
+};
